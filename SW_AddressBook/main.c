2ea52a648ab2be107cdfefded6bcb4dd3a62917b
Jerry-VW <jerryviewon@gmail.com>
2017-01-26 10:47:01 +0900

    creation. first commit, add explanation.

--- SW_AddressBook/main.c
+++ SW_AddressBook/main.c
@@ -5,6 +5,10 @@
 //  Created by WANGWENQIAN on 2017/01/26.
 //  Copyright © 2017 WANGWENQIAN. All rights reserved.
 //
+//  住所録：連番、名前、郵便番号、住所、電話番号
+//  address.binで保存、データ表示、修正、保存などできる
+//
+
 
 #include <stdio.h>
 
